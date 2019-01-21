66568bc042571f8dc40694f2abeb6c170f6ac9e1
Kari Ferenczy <karbear2801@gmail.com>
2019-01-21 22:57:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string){
 }
 
 var uppercase = "HELLO";
-var lowercase = "hello!";
+var lowercase = "hello";
 
 function sayHiToGrandma(string){
   if(string.toUpperCase() === uppercase){
     return("YES INDEED!")
   }
-  if(string.toLowerCase() === lowercase){
+  if(string.toLowerCase() === string){
     return("I can't hear you!")
   }
   if(string === 'I love you, Grandma.'){
