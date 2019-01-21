e056a4a3a28b72400867c2504b9c50c1144b0cd6
Kari Ferenczy <karbear2801@gmail.com>
2019-01-21 22:52:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-var uppercase = "HELLO";
-var lowercase = "hello";
+uppercase = "HELLO";
+lowercase = "hello";
 
 function sayHiToGrandma(string){
   if(string.toUpperCase() === uppercase){
